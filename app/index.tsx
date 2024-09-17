ac9e16c06c90704e1771aa7788c60d71b4015820
Catalina <catalinacardenas459@gmail.com>
2024-09-17 07:06:43 -0500

    se hizo solo el index

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { images } from "../constants";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>!Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={styles.link}>Go to profile</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="w-full h-[300px] max-w-[380px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless possibilities with{''}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless exploration with Aora
+          </Text>
+
+          <CustomButton 
+            tittle="Continue widh Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor = '#161622' style='light'/>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  title: {
-    fontSize: 24, // equivalente a text-3xl
-  },
-  link: {
-    color: 'blue',
-  },
-});
